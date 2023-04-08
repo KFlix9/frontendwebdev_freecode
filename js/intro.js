b22d99bc2a60762ce40044280c39b5c7d9dd3356
Breitsch <D063993@vw9kcfg6p1.speedport.ip>
2023-04-08 10:01:50 +0200

    new stuff

---
+++ js/intro.js
@@ -0,0 +1,6 @@
+const myArray = [10, 20, 30, 40];
+console.log(myArray);
+myArray.push(50); 
+console.log(myArray); 
+// stuff we won't cover, due to no relevance for web dev
+console.log(myArray.reverse());
---
+++ js/js_variables.js
@@ -0,0 +1,6 @@
+const yourFirstVariable = 'Learning to code gives you superpowers';
+const yourSecondVariable = 10; 
+// object
+const yourThirdVariabel = {firstProperty: 'hello world'}; 
+
+console.log(yourThirdVariabel);
