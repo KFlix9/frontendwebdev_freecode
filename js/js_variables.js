6e54a105d1c0077b3d3e71fb9350290bb3fcbb93
Breitsch <D063993@vw9kcfg6p1.speedport.ip>
2023-04-08 10:06:13 +0200

    new commit

--- js/js_variables.js
+++ js/js_variables.js
@@ -1,6 +1,6 @@
 const yourFirstVariable = 'Learning to code gives you superpowers';
 const yourSecondVariable = 10; 
 // object
-const yourThirdVariabel = {firstProperty: 'hello world'}; 
+const yourThirdVariable= {firstProperty: 'hello world'}; 
 
-console.log(yourThirdVariabel);
+console.log(yourThirdVariable);
