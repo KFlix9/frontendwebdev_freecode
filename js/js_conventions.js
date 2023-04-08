fcff29a700854ea45b31885c95c42a958fed27d3
Breitsch <D063993@vw9kcfg6p1.speedport.ip>
2023-04-08 10:51:14 +0200

    js naming conventions

---
+++ js/js_conventions.js
@@ -0,0 +1,16 @@
+// NAMING CONVENTIONS
+
+// these are basically all possbile
+const yourFirstVariable = 10;       //camelCased
+const your_first_variable = 10;     //snake_cased 
+const YOUR_FIRST_VARIABLE = 10;     //constant_cased
+const yourfirstvariable = 10;       //just lower case 
+const YourFirstVariable = 10;       //pascalCased
+
+// STYLE
+// basically all lower camel case for all, PascalCased for Classes
+// find middle ground for naming, not to short and not too long (obviously)
+let q = 20;                     // what is q supposed to be?
+let propertyTaxRateFactor = 20; // better
+
+const firstVariable = 'JS gives you superpowers'; 
