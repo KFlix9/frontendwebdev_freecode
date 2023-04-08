5a0ad1919c507a18d11eddfe963283498a55c54d
Breitsch <D063993@vw9kcfg6p1.speedport.ip>
2023-04-08 10:28:44 +0200

    removed js_variables.js

--- js/js_variables.js
+++
@@ -1,6 +0,0 @@
-const yourFirstVariable = 'Learning to code gives you superpowers';
-const yourSecondVariable = 10; 
-// object
-const yourThirdVariable= {firstProperty: 'hello world'}; 
-
-console.log(yourThirdVariable);
