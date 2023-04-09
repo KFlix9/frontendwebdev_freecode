aa9c41c8c9370d218a68a919109447649e36a269
Breitsch <D063993@vw9kcfg6p1.speedport.ip>
2023-04-09 10:27:24 +0200

    basics on datatypes

---
+++ js/js_data_types_overview.js
@@ -0,0 +1,17 @@
+// DATA TYPES OVERVIEW (formal)
+const stringValue = 'hello world';          //convention
+const anotherStringValue = "hello world"; 
+
+const numberValue = 20; 
+
+const boolenValue = false; 
+
+
+// COMPLEX DATA TYPES
+const numberArray = [10, 20, 30, 40];
+const mixedArray = [10, 'a string', { prop: 'asdf'}, [1,2]]    //all possible, although unlikely to be encountered such
+
+//access arrays
+console.log(numberArray[3]);
+
+console.log(mixedArray[3][1]);
