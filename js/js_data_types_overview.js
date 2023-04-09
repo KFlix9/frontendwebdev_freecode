648b7cd3b59ea15a9426731dc6ccaf8796b7b48d
Breitsch <D063993@vw9kcfg6p1.speedport.ip>
2023-04-09 10:35:00 +0200

    basic data types overview

--- js/js_data_types_overview.js
+++ js/js_data_types_overview.js
@@ -1,11 +1,8 @@
 // DATA TYPES OVERVIEW (formal)
 const stringValue = 'hello world';          //convention
 const anotherStringValue = "hello world"; 
-
 const numberValue = 20; 
-
 const boolenValue = false; 
-
 
 // COMPLEX DATA TYPES
 const numberArray = [10, 20, 30, 40];
@@ -13,5 +10,29 @@ const mixedArray = [10, 'a string', { prop: 'asdf'}, [1,2]]    //all possible, a
 
 //access arrays
 console.log(numberArray[3]);
-
 console.log(mixedArray[3][1]);
+
+
+// OBJECTS
+const objectVariable = {
+    prop1: 20, 
+    prop2: 50, 
+}; 
+
+// acces with dot-notation
+objectVariable.prop1
+
+const nestedObjects = {
+    layer1: {
+        layer2: {
+            layer3: { 
+                targetValue: 30
+            }
+        }
+    }
+}
+
+console.log(nestedObjects.layer1.layer2.layer3.targetValue)
+
+//also possible
+objectVariable['prop1'];
