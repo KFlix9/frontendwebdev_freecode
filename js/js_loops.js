04c597dd16eee4cb3e51fa9a4a9722e0320c2226
Breitsch <D063993@vw9kcfg6p1.fritz.box>
2023-04-20 23:00:44 +0200

    addiing loops intro

---
+++ js/js_loops.js
@@ -0,0 +1,22 @@
+// LOOPS
+
+const blogPosts = [
+    { 
+        title: 'Hello World', 
+        author: 'Felix B.',
+        publishDate: '2023-04-19', 
+        content: 'some stuff' 
+    },
+    { 
+        title: 'This is some nice title', 
+        author: 'Felix B.',
+        publishDate: '2023-04-19', 
+        content: 'this is some nice content' 
+    },
+    { 
+        title: 'I have not yet mastered vim', 
+        author: 'Felix B.',
+        publishDate: '2023-04-19', 
+        content: 'this stuff needs some high key accuracy' 
+    }
+]
