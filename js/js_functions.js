e8dd1fc0e3eca544059f6adf1ba09a2dc1c3924d
Breitsch <D063993@vw9kcfg6p1.fritz.box>
2023-04-21 22:36:15 +0200

    function intro

---
+++ js/js_functions.js
@@ -0,0 +1,25 @@
+// FUNCTIONS
+// basic function
+function myFunction() {
+    console.log('Hello world, this is my first function');
+}
+
+console.log(typeof myFunction); 
+
+myFunction();
+
+// immidiately invoked function
+// ...declared and called at the same time
+( function anotherFunction() { 
+    console.log('immidiately invoked function');
+} ) (); 
+
+// parameters and arguments
+function someFunction(param1, param2) { 
+    console.log(param1); 
+    console.log(param2); 
+}; 
+
+// invoke function
+someFunction(20, 'some string'); 
+
