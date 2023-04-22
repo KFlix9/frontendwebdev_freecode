f2f79d4478c3f3bb747aafedbbe57cd71466aa95
Breitsch <D063993@vw9kcfg6p1.fritz.box>
2023-04-22 22:12:58 +0200

    arrow functions and return

--- js/js_functions.js
+++ js/js_functions.js
@@ -22,3 +22,47 @@ function someFunction(param1, param2) {
 
 // invoke function
 someFunction(20, 'some string'); 
+
+
+// with a variable, which is at least needed when calling function
+const anotherFunction = function () {
+    console.log('another thing');
+}
+
+console.log( typeof anotherFunction);
+
+// (!) the de-facto standard of writing js functions - ES6 Standard - Arrow Function
+// ..this improves readability, seperation and scoping > especially with keyword this - what is covered later
+const arrowFunction = () => {
+    console.log('i am an arrow function'); 
+}
+
+// basic syntac
+() => {
+    
+}
+
+// return values from functions
+const aFunctionWithReturn = () => {
+    return 20;
+}
+const result = aFunctionWithReturn(); 
+console.log(result);
+
+// lets see what happens after return
+const myFunctionNumber = () => {
+    let myNumber = 20;
+    return myNumber;
+    myNumber = 50;              //useless
+}
+
+const myNumber = 20; 
+
+let someReturnFunction = () => {
+    if(myNumber) {
+        return 'returned the function early'; 
+    }
+    return 40;
+}
+
+console.log(someReturnFunction()); 
