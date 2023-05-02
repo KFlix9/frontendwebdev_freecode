97227eabd36b0cea0f106a1315a19305164fb5c3
Felix <felix.breitsch@sap.com>
2023-05-02 21:43:30 +0200

    extending beginner challenges

--- js/beginner_challenges.js
+++ js/beginner_challenges.js
@@ -40,8 +40,67 @@ console.log(returnString(string));
 
 // #4 create function to determine number being "Even" or "Odd"
 function determineOddEven(number) { 
-    return number % 2 === 0 ? "Even" : "Odd";
+    return number % 2 ? "Odd" : "Even";
 }
 
 console.log(determineOddEven(11));
 console.log(determineOddEven(12));
+
+// #5 consider an array of sheeps, count number of sheeps present
+
+const sheeps = [true, true, true, false, 
+                true, true, true, true, 
+                true, false, true, false, 
+                true, false, false, true, 
+                true, true, true, true, 
+                false, false, true, true ];
+
+function countTrue(arr) {
+    let counter = 0;
+    arr.forEach(element => {
+       if(element === true) counter++; 
+    });    
+    return counter;
+}
+
+console.log(countTrue(sheeps));
+
+// #5 alternative: count a, e, i, o, u in a string
+const testString = 'ene mena misto'     // should be 7
+
+function vowelsInString(string) {
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    let counter = 0; 
+
+    vowels.forEach(element => {
+       string.split("").forEach(stringelement => {
+        element === stringelement ? counter++ : null;
+       }) 
+    });
+    return counter;
+}
+
+console.log(vowelsInString(testString));
+
+// #6 Jenny secret message
+
+let wrongName = 'Heinz';
+let rightName = 'Johnny'; 
+
+function greet(name){
+    if(name === "Johnny"){
+        return "Hello, my love!";
+    } else {
+        return "Hello, " + name + "!";
+    }; 
+}
+
+console.log(greet(wrongName)); 
+
+// #7 check if number is divisible by x and y
+
+function isDivisble(n, x, y) {
+    return n % x === 0 && n % y === 0 ? true : false; 
+}
+
+console.log(isDivisble(3,1,3));
