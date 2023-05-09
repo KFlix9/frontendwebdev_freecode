af5756d008c67aaff8d894d399b4fc3591fcbf57
Felix <felix.breitsch@sap.com>
2023-05-09 23:21:02 +0200

    basics / play with primitives

---
+++ js/primitives.js
@@ -0,0 +1,14 @@
+// PRIMITIVES
+
+// ...basically the simplest form of a data type = THE data types
+
+
+const string = new String("hello");  //this actually should not really be called / defined somewhere - js takes care
+const stringOther = 'hello';
+
+console.log(string === stringOther);
+console.log(string == stringOther);
+
+// this actually works - wrapping in js object
+console.log('some string'.toUpperCase());
+
